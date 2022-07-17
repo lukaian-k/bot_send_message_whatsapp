8c38c379a845c06cd85670f661c392e6ed4ce3fa
Lucas José <lukinhas8979@gmail.com>
2022-07-17 18:16:27 -0300

    melhoria no codigo // mudando para ingles os nomes das variaveis e arquivos

--- function.py
+++ function.py
@@ -1,41 +1,24 @@
 import pywhatkit as pwk
 
 
-def send_message(hora, lista_numeros_site, mensagem_site, lista_numeros_outros_servicos, mensagem_outros_servicos):
+def send_message(time, list_numbers, messages):
     print('Verificando os contatos que irão ser contactados...\n\n')
-    if hora[0] > 23:
-        hora[0] = 0
+    if time['hour'] > 23:
+        time['hour'] = 0
 
-    if lista_numeros_site:
-        print('Preparando os envios de propostas de sites para os contatos...')
-        for i in lista_numeros_site:
-            if hora[1] >= 59:
-                if hora[0] >= 23:
-                    hora[0] = 0
+    print('Iniciando os envios das propostas para os contatos...')
+    for key in list_numbers.keys():
+        for number in list_numbers[key]:
+            if time['minute'] >= 59:
+                if time['hour'] >= 23:
+                    time['hour'] = 0
                 else:
-                    hora[0] += 1
-                hora[1] = 0
+                    time['hour'] += 1
+                time['minute'] = 0
             else:
-                hora[1] += 1
+                time['minute'] += 1
 
-            print(f'\nEnviado para o contato: {i} - horário: {hora[0]}:{hora[1]}')
-            pwk.sendwhatmsg(i, mensagem_site, hora[0], hora[1], 10, True, 13)
-    else:
-        print('\nNão ha nenhum contato para envio da proposta de site.\n')
-
-    if lista_numeros_outros_servicos:
-        print('Preparando os envios de propostas de outros serviços para os contatos...')
-        for i in lista_numeros_outros_servicos:
-            if hora[1] >= 59:
-                if hora[0] >= 23:
-                    hora[0] = 0
-                else:
-                    hora[0] += 1
-                hora[1] = 0
-            else:
-                hora[1] += 1
-
-            print(f'\nEnviado para o contato: {i} - horário: {hora[0]}:{hora[1]}')
-            pwk.sendwhatmsg(i, mensagem_outros_servicos, hora[0], hora[1], 10, True, 13)
-    else:
-        print('\nNão ha nenhum contato para envio da proposta de outros serviços.\n')
+            print(f'\nEnviado para o contato: {number} - horário: {time["hour"]}:{time["minute"]}')
+            pwk.sendwhatmsg(number, messages[key], time['hour'], time['minute'], 10, True, 13)
+    
+    print('\nNão existe ou acabou os contatos para contactar.\n')
--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from function import send_message
 from datetime import datetime
 
 
-nome = 'Lucas José' #INSIRA SEU NOME AQUI
+name = 'Lucas José' #ENTER YOUR NAME HERE
+waiting_minutes = 0 #SET HOW MANY MINUTES AFTER MESSAGES WILL BE SENT
 
 print('\n<<<Programa Inicializado>>>\n')
-file = open('contatos/lista_numeros_site.txt', 'r+')
-lista_numeros_site = file.readlines(); file.truncate(0), file.close()
+file = open('contacts/offer_websites.txt', 'r+')
+list_numbers = {'01_message': file.readlines()}; file.truncate(0), file.close()
 
-file = open('contatos/lista_numeros_outros_servicos.txt', 'r+')
-lista_numeros_outros_servicos = file.readlines(); file.truncate(0), file.close()
+file = open('contacts/offer_other_services.txt', 'r+')
+list_numbers['02_message'] = file.readlines(); file.truncate(0), file.close()
 print('Arquivos limpos!\n')
 
-horario = datetime.now(); hora = [int(horario.strftime('%H')), int(horario.strftime('%M'))]
+time = datetime.now(); time = {'hour': int(time.strftime('%H')), 'minute': int(time.strftime('%M'))+waiting_minutes}
 
-mensagem_site = f'Olá, bom dia, chamo-me {nome} e sou colaborador da Include Jr. Após algumas pesquisas, notamos que vocês não possuem um site próprio. Devido a isso, gostaríamos de indicar os nossos serviços e de verificar a possibilidade de marcar uma reunião para apresentarmos uma proposta de estruturação do site.\n\n*Agradecemos pela atenção, e desejamos um ótimo dia.*'
-mensagem_outros_servicos = f'Olá, bom dia, chamo-me {nome} e sou colaborador da Include Jr. Após algumas pesquisas, constatamos que vocês possuem um site próprio, e concluímos que a importância de fazer investimentos em soluções tecnológicas já é uma prática bastante difundida na organização. Por conseguinte, queremos auxiliá-los a ter um crescimento ainda maior do negócio através de outros tipos de tecnologias, desde sistemas informacionais a aplicativos mobile. Caso haja interesse em nossos serviços, gostaríamos de verificar a possibilidade de marcarmos uma reunião para discussão dos termos e definição de metas.\n\n*Agradecemos pela atenção, e desejamos um ótimo dia.*'
+messages = {
+'01_message': f'Olá, bom dia, chamo-me {name} e sou colaborador da Include Jr. Após algumas pesquisas, notamos que vocês não possuem um site próprio. Devido a isso, gostaríamos de indicar os nossos serviços e de verificar a possibilidade de marcar uma reunião para apresentarmos uma proposta de estruturação do site.\n\n*Agradecemos pela atenção, e desejamos um ótimo dia.*',
+'02_message': f'Olá, bom dia, chamo-me {name} e sou colaborador da Include Jr. Após algumas pesquisas, constatamos que vocês possuem um site próprio, e concluímos que a importância de fazer investimentos em soluções tecnológicas já é uma prática bastante difundida na organização. Por conseguinte, queremos auxiliá-los a ter um crescimento ainda maior do negócio através de outros tipos de tecnologias, desde sistemas informacionais a aplicativos mobile. Caso haja interesse em nossos serviços, gostaríamos de verificar a possibilidade de marcarmos uma reunião para discussão dos termos e definição de metas.\n\n*Agradecemos pela atenção, e desejamos um ótimo dia.*'
+}
 
-send_message(hora, lista_numeros_site, mensagem_site, lista_numeros_outros_servicos, mensagem_outros_servicos)
+send_message(time, list_numbers, messages)
